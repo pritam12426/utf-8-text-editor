c76a66bdcfbd52233119aadefc8c30d2edbbf242
Pritam <84720825+pritam12426@users.noreply.github.com>
2024-07-24 15:19:33 +0530

    Error handling && Sections

--- src/main.c
+++ src/main.c
@@ -1,47 +1,112 @@
+/*** includes ***/
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 
-static struct termios orig_termios;
+/*** defines ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 
+/*** data ***/
+struct editorConfig {
+  struct termios orig_termios;
+};
+struct editorConfig E;
+
+
+/*** function prototype ***/
+void die(const char *s);
+void disableRawMode(void);
+void enableRawMode(void);
+void editorProcessKeypress();
+char editorReadKey();
+void editorRefreshScreen();
+
+void editorDrawRows() {
+  int y;
+  for (y = 0; y < 24; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
+
+/*** terminal ***/
 void disableRawMode(void) {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {
+    die("tcsetattr");
+  }
+}
+
+
+char editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+
+
+/*** input ***/
+void editorProcessKeypress() {
+  char c = editorReadKey();
+  switch (c) {
+    case (CTRL_KEY('q')): {
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
+      exit(0);
+      break;
+    }
+  }
+}
+
+/*** output ***/
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  editorDrawRows();
+
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+
+void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  perror(s);
+  exit(1);
 }
 
 
 void enableRawMode(void) {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) { die("tcgetattr"); }
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   raw.c_cc[VMIN]  = 0;
-  raw.c_cc[VTIME] = 5;
+  raw.c_cc[VTIME] = 1;
 
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+/*** init ***/
 
 int main(void) {
   enableRawMode();
 
-  char c = 0;
   while (1) {
-    char c = '\0';
-    read(STDIN_FILENO, &c, 1);
-    if (c == 'q') break;
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
+    editorProcessKeypress();
+    editorRefreshScreen();
   }
 
   return 0;
