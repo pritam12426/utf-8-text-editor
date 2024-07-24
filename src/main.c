cfcef847f8e1069783facac3352b2696aee72ea8
Pritam <84720825+pritam12426@users.noreply.github.com>
2024-07-24 12:28:06 +0530

    Turn Off All Output Processing
    It turns out that the terminal does a similar translation
    on the output side. It translates each newline ("\n") we
    print into a carriage return followed by a newline ("\r\n").
    The terminal requires both of these characters in order to
    start a new line of text. The carriage return moves the
    cursor back to the beginning of the current line, and the
    newline moves the cursor down a line, scrolling the
    screen if necessary.

--- src/main.c
+++ src/main.c
@@ -18,7 +18,9 @@ void enableRawMode(void) {
   atexit(disableRawMode);
   struct termios raw = orig_termios;
   tcgetattr(STDIN_FILENO, &raw);
-  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -30,9 +32,9 @@ int main(void) {
   char c = 0;
   while ((read(STDIN_FILENO, &c, 1) == 1) && (c != 'q')) {
     if (iscntrl(c)) {
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     } else {
-      printf("%d ('%c')\n", c, c);
+      printf("%d ('%c')\r\n", c, c);
     }
   }
 
