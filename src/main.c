c678ebd3db4a42fc05b9a33202c8533780792ea7
Pritam <84720825+pritam12426@users.noreply.github.com>
2024-07-24 12:19:38 +0530

    Disable `Ctrl-S` and `Ctrl-Q`
    Ctrl-S stops data from being transmitted to the terminal until you press Ctrl-Q.
    This originates in the days when you might want to pause the transmission of
    data to let a device like a printer catch up. Let’s just turn off that feature.

--- src/main.c
+++ src/main.c
@@ -18,7 +18,8 @@ void enableRawMode(void) {
   atexit(disableRawMode);
   struct termios raw = orig_termios;
   tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+  raw.c_iflag &= ~(IXON);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
