5c72d1478c2708818534e8406f26db10f883ce0f
Pritam <84720825+pritam12426@users.noreply.github.com>
2024-07-24 12:12:57 +0530

    Turn Off Ctrl-C And Ctrl-Z Signals
    By default, Ctrl-C sends a `SIGINT` signal to the current process which
    causes it to terminate, and Ctrl-Z sends a SIGTSTP signal to the current
    process which causes it to suspend. Let’s turn off the sending of both
    of these signals.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
 
-struct termios orig_termios;
+static struct termios orig_termios;
 
 
 void disableRawMode(void) {
@@ -16,7 +18,7 @@ void enableRawMode(void) {
   atexit(disableRawMode);
   struct termios raw = orig_termios;
   tcgetattr(STDIN_FILENO, &raw);
-  raw.c_lflag &= ~(ECHO);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -26,7 +28,11 @@ int main(void) {
 
   char c = 0;
   while ((read(STDIN_FILENO, &c, 1) == 1) && (c != 'q')) {
-    ;
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
   }
 
   return 0;
